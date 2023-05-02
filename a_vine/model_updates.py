e5f27483585df508896c51b643f9fa53cfa2326f
James <james.ch.pino@gmail.com>
2023-05-02 14:16:48 -0400

    Adding biolog exchange reactions to model.

--- a_vine/model_updates.py
+++ a_vine/model_updates.py
@@ -12,6 +12,8 @@ import cobra
 import os
 import logging
 from concerto.helpers.id_mappers import update_ids
+from concerto.utils.biolog_help import add_biolog_exchanges
+
 
 log = logging.getLogger()
 
@@ -19,18 +21,18 @@ _file_path = os.path.dirname(__file__)
 starting_model_f_name = 'iDT1278.xml'
 s_model_path = os.path.join(_file_path, starting_model_f_name)
 
-model = cobra.io.read_sbml_model(s_model_path)
-model.id = "AV"
+starting_model = cobra.io.read_sbml_model(s_model_path)
+starting_model.id = "AV"
 
 output_model_name = 'azo_vine.xml'
 output_model_path = os.path.join(_file_path, output_model_name)
 
 
-def write_model():
+def write_model(model):
     cobra.io.write_sbml_model(model, output_model_path)
 
 
-def update_1():
+def update_1(model):
     # updates bug in compartment of the model
     log.info("Updating compartments")
     for metabolite in model.metabolites:
@@ -39,19 +41,29 @@ def update_1():
             metabolite.id = metabolite.id[:-2]
             # rename compartment
             metabolite.compartment = metabolite.id[-1]
+    return model
 
 
-def update_2():
+def update_2(model):
     log.info('Adding annotations to metabolites')
 
     update_ids(model)
+    return model
+
+
+def update_3(model):
+    # add missing biolog reactions to model
+    log.info("Adding biolog exchange reactions to prefix")
+    model = add_biolog_exchanges(model)
+    return model
 
 
 def update_model():
     # Fix compartments
-    update_1()
-    update_2()
-    write_model()
+    model = update_1(starting_model)
+    model = update_2(model)
+    model = update_3(model)
+    write_model(model)
 
 
 if __name__ == '__main__':
